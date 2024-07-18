b5c334320f7198e4c04246f3c39f375a2b610583
sharjeel <sharjeelkhatri303@gmail.com>
2024-07-18 11:19:09 +0500

    complete

--- app.js
+++ app.js
@@ -23,6 +23,7 @@
     const date = document.querySelector("#date")
     const time = document.querySelector("#time")
     const cond = document.querySelector("#cond")
+    const img = document.querySelector("#condImg")
     
     
     
@@ -30,6 +31,7 @@
     const cityquery = document.querySelector("#cityquery")
     const condquery = document.querySelector("#condquery")
     const datequery = document.querySelector("#datequery")
+    const imgquery = document.querySelector("#imgquery")
     
 
     const day1 = document.querySelector("#day1")
@@ -57,6 +59,70 @@
 
 
     const errordiv = document.querySelector("#errordiv")
+
+
+//     document.addEventListener("DOmContentLoaded" , async () => {
+
+    
+        
+//         let promise = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=2a9d4ec9538442959fc122840240206&q=karachi&days=7`)
+//         return await promise.json()
+        
+        
+        
+        
+//     console.log(defaultcity()); 
+//     let a = defaultcity()
+    
+//     let Celcius = temp.innerHTML = `${a.current.temp_c}`
+//     let floor = Math.floor(Celcius)
+//     temp.innerHTML = floor + "°C"
+
+//     pressure.innerHTML = `Air-Pressure: ${result.current.wind_kph} kph`
+//     sunrise.innerHTML = `Sunrise: ${result.forecast.forecastday[0].astro.sunrise}`
+//     sunset.innerHTML = `Sunset: ${result.forecast.forecastday[0].astro.sunset}`
+
+//     city.innerHTML = `${result.location.name}`
+//     date.innerHTML = `${result.location.localtime.split(' ')[0]}`
+//     time.innerHTML = `${result.location.localtime.split(' ')[1]}`
+//     // cond.innerHTML = `${result.current.condition.text}`
+//     country.innerHTML = `${result.location.country}`
+
+
+//     condquery.innerHTML = `${result.current.condition.text}`
+//     cityquery.innerHTML = `${result.location.name}`
+//     tempquery.innerHTML = floor + "°C"
+//     datequery.innerHTML = `${result.location.localtime.split(' ')[0]}`
+
+
+    
+    
+//     const days = [day1 ,day2 ,day3 ,day4 ,day5 ,day6, day7 ]
+//     const daysMin = [day1min, day2min, day3min, day4min, day5min, day6min, day7min]
+//     const daysMax = [day1max, day2max, day3max, day4max, day5max, day6max, day7max]
+    
+//     result.forecast.forecastday.forEach((forecastDay, index) => {
+//         daysMin[index].innerHTML = `Min <br>${Math.floor(forecastDay.day.mintemp_c)}°C`
+//         daysMax[index].innerHTML = `Max <br>${Math.floor(forecastDay.day.maxtemp_c)}°C`
+//         days[index].innerHTML = `${forecastDay.date}`
+//     })
+// })
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
     
 
     async function getdata(cityName){
@@ -65,19 +131,15 @@
             load.style.display = "block";
             body.style.display = "none";
             errordiv.style.display = "none"
+            country.innerHTML = "Country Name"
 
 
-            // load.innerHTML = `loading`
-            // const days = [day1 ,day2 ,day3 ,day4 ,day5 ,day6, day7 ]
-            // const daysMin = [day1min, day2min, day3min, day4min, day5min, day6min, day7min]
-            // const daysMax = [day1max, day2max, day3max, day4max, day5max, day6max, day7max]
-
-            // days[index] = ''
-            // daysMin[index] = ''
-            // daysMax[index] = ''
         // let cityName = "karachi"
             
-        let promise = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=2a9d4ec9538442959fc122840240206&q=${cityName}&days=7`)
+        // let promise = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=2a9d4ec9538442959fc122840240206&q=${cityName}&days=7`)
+        let promise =
+          await fetch(`https://api.weatherapi.com/v1/forecast.json?key=84ea4837f2924ab7bad114838240807&q=${cityName}&days=7`);
+            
             
         if (!promise.ok) {
             throw new Error(`City not found`)
@@ -97,6 +159,7 @@
 }
 
 
+
     form.addEventListener("submit" , async (event) =>{
         event.preventDefault()    
         let value = input.value.trim()
@@ -107,7 +170,7 @@
 
                 
             let result = await getdata(value)
-            // console.log(result);
+            console.log(result);
 
             let Celcius = temp.innerHTML = `${result.current.temp_c}`
             let floor = Math.floor(Celcius)
@@ -121,10 +184,13 @@
             date.innerHTML = `${result.location.localtime.split(' ')[0]}`
             time.innerHTML = `${result.location.localtime.split(' ')[1]}`
             cond.innerHTML = `${result.current.condition.text}`
+            img.src = `${result.current.condition.icon}`
             country.innerHTML = `${result.location.country}`
+
 
     
             condquery.innerHTML = `${result.current.condition.text}`
+            imgquery.src = `${result.current.condition.icon}`
             cityquery.innerHTML = `${result.location.name}`
             tempquery.innerHTML = floor + "°C"
             datequery.innerHTML = `${result.location.localtime.split(' ')[0]}`
@@ -144,12 +210,13 @@
 
             input.value = ""
             body.style.display = "block";
-
-        }catch(error){
-            errordiv.innerHTML = `<p>City Not Found</p>`;
-            errordiv.style.display = "block";
-            console.log(error);
-            input.value = ""
+            
+            }catch(error){
+                errordiv.innerHTML = `<p>City Not Found</p>`;
+                errordiv.style.display = "block";
+                console.log(error);
+                // country.innerHTML = "Country Name"
+            // input.value = ""
 
         }
         
